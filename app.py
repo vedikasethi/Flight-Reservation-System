f17e794359bbee5363df1f70e3b57c5555b19713
vedikasethi <63193360+vedikasethi@users.noreply.github.com>
2024-10-25 01:42:33 +0000

    Made a few small corrections

--- app.py
+++ app.py
@@ -8,10 +8,5 @@ def hello_world():
     return render_template('home.html')
 
 
-@app.route('/negi')
-def Print_Negi():
-    return "negi"
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
