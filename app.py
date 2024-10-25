cdc8c1d45e44049a0f40623e5d3772ffa709fa3d
vedikasethi <63193360+vedikasethi@users.noreply.github.com>
2024-10-25 01:25:16 +0000

    Added Basic Frontend HTML and CSS, data on Excel

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html')
+
+
+@app.route('/negi')
+def Print_Negi():
+    return "negi"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
